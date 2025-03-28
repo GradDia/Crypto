79cb5efa96b09e3ad4117d55174f72380de8dcab
GradDia <oprish-2000@mail,ru>
2025-03-28 23:03:12 +0300

    last fix 2 it

--- internal/cases/service_test.go
+++ internal/cases/service_test.go
@@ -21,17 +21,18 @@ func Test_GetLastRates_Success(t *testing.T) {
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
 
 	titles := []string{"BTC", "ETH"}
 	expectedCoins := []entities.Coin{
 		{CoinName: "BTC", Price: 28000},
 		{CoinName: "ETH", Price: 1500},
 	}
+
+	mockStorage.EXPECT().
+		GetCoinsList(gomock.Any()).
+		Return([]string{"BTC", "ETH"}, nil)
+
 	mockStorage.EXPECT().
 		GetActualCoins(gomock.Any(), titles).
 		Return(expectedCoins, nil)
@@ -40,7 +41,6 @@ func Test_GetLastRates_Success(t *testing.T) {
 	require.NoError(t, err)
 
 	coins, err := service.GetLastRates(context.Background(), titles)
-
 	assert.NoError(t, err)
 	assert.Equal(t, expectedCoins, coins)
 }
@@ -52,198 +52,81 @@ func Test_GetLastRates_Error(t *testing.T) {
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
 
 	titles := []string{"BTC", "ETH"}
+
+	mockStorage.EXPECT().
+		GetCoinsList(gomock.Any()).
+		Return([]string{"BTC", "ETH"}, nil)
+
 	mockStorage.EXPECT().
 		GetActualCoins(gomock.Any(), titles).
-		Return(nil, entities.ErrInvalidParam) // Возвращаем базовую ошибку
+		Return(nil, entities.ErrInvalidParam)
 
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err) // Проверяем, что сервис создан без ошибок
+	require.NoError(t, err)
 
 	coins, err := service.GetLastRates(context.Background(), titles)
-
 	assert.Error(t, err)
 	assert.Nil(t, coins)
-	assert.ErrorIs(t, err, entities.ErrInvalidParam) // Проверяем тип ошибки
+	assert.ErrorIs(t, err, entities.ErrInvalidParam)
 }
 
-func Test_GetMaxRates_Success(t *testing.T) {
+func Test_GetRatesWithAgg_Success(t *testing.T) {
 	t.Parallel()
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
 
 	titles := []string{"BTC", "ETH"}
 	expectedCoins := []entities.Coin{
 		{CoinName: "BTC", Price: 30000},
 		{CoinName: "ETH", Price: 2000},
 	}
+
 	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
+		GetCoinsList(gomock.Any()).
+		Return([]string{"BTC", "ETH"}, nil)
+
+	mockStorage.EXPECT().
+		GetAggregateCoins(gomock.Any(), titles, "max").
 		Return(expectedCoins, nil)
 
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	coins, err := service.GetMaxRates(context.Background(), titles)
-
+	coins, err := service.GetRatesWithAgg(context.Background(), titles, "max")
 	assert.NoError(t, err)
 	assert.Equal(t, expectedCoins, coins)
 }
-func Test_GetMaxRates_Error(t *testing.T) {
+
+func Test_GetRatesWithAgg_Error(t *testing.T) {
 	t.Parallel()
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
 
 	titles := []string{"BTC", "ETH"}
+
 	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
+		GetCoinsList(gomock.Any()).
+		Return([]string{"BTC", "ETH"}, nil)
+
+	mockStorage.EXPECT().
+		GetAggregateCoins(gomock.Any(), titles, "max").
 		Return(nil, entities.ErrInvalidParam)
 
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	coins, err := service.GetMaxRates(context.Background(), titles)
-
-	assert.Error(t, err)
-	assert.Nil(t, coins)
-	assert.ErrorIs(t, err, entities.ErrInvalidParam)
-}
-
-func Test_GetMinRates_Success(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
-
-	titles := []string{"BTC", "ETH"}
-	expectedCoins := []entities.Coin{
-		{CoinName: "BTC", Price: 27000},
-		{CoinName: "ETH", Price: 1400},
-	}
-	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
-		Return(expectedCoins, nil)
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	coins, err := service.GetMinRates(context.Background(), titles)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCoins, coins)
-}
-
-func Test_GetMinRates_Error(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
-
-	titles := []string{"BTC", "ETH"}
-	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
-		Return(nil, entities.ErrInvalidParam)
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	coins, err := service.GetMinRates(context.Background(), titles)
-
-	assert.Error(t, err)
-	assert.Nil(t, coins)
-	assert.ErrorIs(t, err, entities.ErrInvalidParam)
-}
-
-func Test_GetAvgRates_Success(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
-
-	titles := []string{"BTC", "ETH"}
-	expectedCoins := []entities.Coin{
-		{CoinName: "BTC", Price: 28500},
-		{CoinName: "ETH", Price: 1700},
-	}
-	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
-		Return(expectedCoins, nil)
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	coins, err := service.GetAvgRates(context.Background(), titles)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCoins, coins)
-}
-
-func Test_GetAvgRates_Error(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
-
-	titles := []string{"BTC", "ETH"}
-	mockStorage.EXPECT().
-		GetAggregateCoins(gomock.Any(), titles).
-		Return(nil, entities.ErrInvalidParam)
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	coins, err := service.GetAvgRates(context.Background(), titles)
-
+	coins, err := service.GetRatesWithAgg(context.Background(), titles, "max")
 	assert.Error(t, err)
 	assert.Nil(t, coins)
 	assert.ErrorIs(t, err, entities.ErrInvalidParam)
@@ -256,29 +139,34 @@ func Test_ActualizeRates_Success(t *testing.T) {
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
 
-	titles := []string{"BTC", "ETH"}
+	// Определяем поведение мока Storage
+	allTitles := []string{"BTC", "ETH"}
+	mockStorage.EXPECT().
+		GetCoinsList(gomock.Any()).
+		Return(allTitles, nil)
+
+	// Определяем поведение мока CryptoProvider
 	actualRates := []entities.Coin{
 		{CoinName: "BTC", Price: 29000},
 		{CoinName: "ETH", Price: 1600},
 	}
 	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), titles).
+		GetActualRates(gomock.Any(), allTitles).
 		Return(actualRates, nil)
 
-	for _, coin := range actualRates {
-		mockStorage.EXPECT().
-			Store(gomock.Any(), []entities.Coin{coin}).
-			Return(nil)
-	}
+	// Определяем поведение мока Storage для сохранения новых монет
+	mockStorage.EXPECT().
+		Store(gomock.Any(), actualRates).
+		Return(nil)
 
+	// Создаем сервис с моками
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	err = service.ActualizeRates(context.Background(), cases.WithTitles(titles))
-
+	// Вызываем метод ActualizeRates
+	err = service.ActualizeRates(context.Background())
 	assert.NoError(t, err)
 }
 
@@ -289,207 +177,105 @@ func Test_ActualizeRates_CryptoProviderError(t *testing.T) {
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
 
-	titles := []string{"BTC", "ETH"}
+	allTitles := []string{"BTC", "ETH"}
+
+	mockStorage.EXPECT().
+		GetCoinsList(gomock.Any()).
+		Return(allTitles, nil)
+
 	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), titles).
+		GetActualRates(gomock.Any(), allTitles).
 		Return(nil, errors.New("crypto provider error"))
 
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	err = service.ActualizeRates(context.Background(), cases.WithTitles(titles))
-
+	err = service.ActualizeRates(context.Background())
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "crypto provider error")
 }
 
-func Test_ActualizeRates_StorageError(t *testing.T) {
+func Test_GetLastRates_AllCoinsExist(t *testing.T) {
 	t.Parallel()
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockStorage := testdata.NewMockStorage(ctrl)
-
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
 
-	titles := []string{"BTC", "ETH"}
-	actualRates := []entities.Coin{
-		{CoinName: "BTC", Price: 29000},
-		{CoinName: "ETH", Price: 1600},
-	}
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), titles).
-		Return(actualRates, nil)
-
-	for i, coin := range actualRates {
-		if i == 0 { // Первый вызов успешен
-			mockStorage.EXPECT().
-				Store(gomock.Any(), []entities.Coin{coin}).
-				Return(nil)
-		} else { // Второй вызов возвращает ошибку
-			mockStorage.EXPECT().
-				Store(gomock.Any(), []entities.Coin{coin}).
-				Return(errors.New("storage error"))
-		}
-	}
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	err = service.ActualizeRates(context.Background(), cases.WithTitles(titles))
-
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "storage error")
-}
-
-func Test_CheckExistingTitles_Success(t *testing.T) {
-	t.Parallel()
-
-	// Создаем контроллер для моков
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	// Создаем мок для интерфейса Storage
-	mockStorage := testdata.NewMockStorage(ctrl)
-
-	// Определяем поведение мока Storage
+	// Определяем поведение моков
 	requestTitles := []string{"BTC", "ETH"}
 	existingTitles := []string{"BTC", "ETH"}
+	expectedCoins := []entities.Coin{
+		{CoinName: "BTC", Price: 28000},
+		{CoinName: "ETH", Price: 1500},
+	}
+
 	mockStorage.EXPECT().
 		GetCoinsList(gomock.Any()).
 		Return(existingTitles, nil)
 
-	// Создаем мок для интерфейса CryptoProvider
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), gomock.Any()).
-		Return([]entities.Coin{}, nil).AnyTimes()
+	mockStorage.EXPECT().
+		GetActualCoins(gomock.Any(), requestTitles).
+		Return(expectedCoins, nil)
 
-	// Создаем сервис с моками
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	// Вызываем метод checkExistingTitles
-	err = service.CheckExistingTitles(context.Background(), requestTitles)
-
-	// Проверяем результат
+	coins, err := service.GetLastRates(context.Background(), requestTitles)
 	assert.NoError(t, err)
+	assert.Equal(t, expectedCoins, coins)
 }
 
-func Test_CheckExistingTitles_AddMissingCoins(t *testing.T) {
+func Test_GetLastRates_AddMissingCoins(t *testing.T) {
 	t.Parallel()
 
-	// Создаем контроллер для моков
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// Создаем мок для интерфейса Storage
 	mockStorage := testdata.NewMockStorage(ctrl)
-
-	// Создаем мок для интерфейса CryptoProvider
 	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
 
-	// Определяем поведение мока Storage
+	// Определяем поведение моков
 	requestTitles := []string{"BTC", "ETH", "LTC"}
 	existingTitles := []string{"BTC", "ETH"}
-	mockStorage.EXPECT().
-		GetCoinsList(gomock.Any()).
-		Return(existingTitles, nil)
-
-	// Определяем поведение мока CryptoProvider
 	missingTitles := []string{"LTC"}
+
 	newCoins := []entities.Coin{
 		{CoinName: "LTC", Price: 50},
 	}
+
+	mockStorage.EXPECT().
+		GetCoinsList(gomock.Any()).
+		Return(existingTitles, nil)
+
 	mockCryptoProvider.EXPECT().
 		GetActualRates(gomock.Any(), missingTitles).
 		Return(newCoins, nil)
 
-	// Определяем поведение мока Storage для сохранения новых монет
 	for _, coin := range newCoins {
 		mockStorage.EXPECT().
 			Store(gomock.Any(), []entities.Coin{coin}).
 			Return(nil)
 	}
 
-	// Создаем сервис с моками
+	mockStorage.EXPECT().
+		GetActualCoins(gomock.Any(), requestTitles).
+		Return(append([]entities.Coin{
+			{CoinName: "BTC", Price: 28000},
+			{CoinName: "ETH", Price: 1500},
+		}, newCoins...), nil)
+
 	service, err := cases.NewService(mockStorage, mockCryptoProvider)
 	require.NoError(t, err)
 
-	// Вызываем метод checkExistingTitles
-	err = service.CheckExistingTitles(context.Background(), requestTitles)
-
-	// Проверяем результат
+	coins, err := service.GetLastRates(context.Background(), requestTitles)
 	assert.NoError(t, err)
-}
-
-func Test_CheckExistingTitles_CryptoProviderError(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-
-	requestTitles := []string{"BTC", "ETH", "LTC"}
-	existingTitles := []string{"BTC", "ETH"}
-	mockStorage.EXPECT().
-		GetCoinsList(gomock.Any()).
-		Return(existingTitles, nil)
-
-	missingTitles := []string{"LTC"}
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), missingTitles).
-		Return(nil, errors.New("crypto provider error"))
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	err = service.CheckExistingTitles(context.Background(), requestTitles)
-
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "crypto provider error")
-}
-
-func Test_CheckExistingTitles_StorageError(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockStorage := testdata.NewMockStorage(ctrl)
-	mockCryptoProvider := testdata.NewMockCryptoProvider(ctrl)
-
-	requestTitles := []string{"BTC", "ETH", "LTC"}
-	existingTitles := []string{"BTC", "ETH"}
-	mockStorage.EXPECT().
-		GetCoinsList(gomock.Any()).
-		Return(existingTitles, nil)
-
-	missingTitles := []string{"LTC"}
-	newCoins := []entities.Coin{
-		{CoinName: "LTC", Price: 50},
-	}
-	mockCryptoProvider.EXPECT().
-		GetActualRates(gomock.Any(), missingTitles).
-		Return(newCoins, nil)
-
-	for _, coin := range newCoins {
-		mockStorage.EXPECT().
-			Store(gomock.Any(), []entities.Coin{coin}).
-			Return(errors.New("storage error"))
-	}
-
-	service, err := cases.NewService(mockStorage, mockCryptoProvider)
-	require.NoError(t, err)
-
-	err = service.CheckExistingTitles(context.Background(), requestTitles)
-
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "storage error")
+	assert.Equal(t, append([]entities.Coin{
+		{CoinName: "BTC", Price: 28000},
+		{CoinName: "ETH", Price: 1500},
+	}, newCoins...), coins)
 }
